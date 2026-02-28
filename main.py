cc308ee029392e9ec263e643827ebd108e42f2d4
haqinzmamul12 <haqinzmamul12@gmail.com>
2026-02-28 17:29:32 +0530

    feat: Modify predict api

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def health():
 
 @app.post("/predict")
 def predict(data: InputData):
-    result = data.feature1 * 3 + data.feature2
+    result = data.feature1 * 10 + data.feature2
     print(f"Prediction requested: {data}")   # 👈 new logging
     return {"prediction": result}
 
